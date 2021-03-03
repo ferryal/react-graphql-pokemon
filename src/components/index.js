9401ab4e556712f389c19ea15e3fc442131f0fdd
ferryal <ferryal.bahari@amartha.com>
2021-03-03 21:31:28 +0700

    feat: add component modal

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,4 @@ export { default as Loading } from './Loading';
 export { default as Button } from './Button';
 export { default as Card } from './Card';
 export { default as Badge } from './Badge';
+export { default as Modal } from './Modal';

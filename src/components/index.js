45fc3938b7f73ef7c572214fac483f355a6a67da
ferryal <ferryal.bahari@amartha.com>
2021-03-03 09:23:11 +0700

    feat: export components

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@
 export { default as Layout } from './Layout';
 export { default as Navbar } from './Navbar';
 export { default as Loading } from './Loading';
+export { default as Button } from './Button';
+export { default as Card } from './Card';
